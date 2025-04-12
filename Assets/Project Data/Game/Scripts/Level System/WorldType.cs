dab1e119b8fb853a603312e78f7d5ba5aca39136
NikitaDalal3 <chindhudalal123@gmail.com>
2025-04-12 22:31:30 +0530

    InitialCommit

---
+++ Assets/Project Data/Game/Scripts/Level System/WorldType.cs
@@ -0,0 +1,12 @@
+﻿namespace Watermelon.LevelSystem
+{
+    [System.Serializable]
+    public enum WorldType
+    {
+        World_01 = 1,
+        World_02 = 2,
+        World_03 = 3,
+        World_04 = 4,
+        World_05 = 5,
+    }
+}
